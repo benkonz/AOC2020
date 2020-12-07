456797bc0f59053452343a2c6b92b105ca0460f4
Ben Konz <benkonz@protonmail.com>
2020-12-06 23:56:20 -0500

    day 5 part 2

--- lib/day5.ex
+++ lib/day5.ex
@@ -28,9 +28,8 @@ defmodule AOC.Day5 do
   def part1(input) do
     input
     |> Enum.reduce(0, fn seat, acc ->
-      row = String.slice(seat, 0..6) |> IO.inspect() |> bsp(0, 0, 127)
-      col = String.slice(seat, 7..-1) |> IO.inspect() |> bsp(0, 0, 7)
-      IO.inspect({row, col})
+      row = String.slice(seat, 0..6) |> bsp(0, 0, 127)
+      col = String.slice(seat, 7..-1) |> bsp(0, 0, 7)
       seat_id = row * 8 + col
 
       if seat_id > acc do
@@ -42,5 +41,30 @@ defmodule AOC.Day5 do
   end
 
   def part2(input) do
+    total_seats =
+      0..7
+      |> Enum.to_list()
+      |> Enum.reduce(MapSet.new(), fn col, acc ->
+        0..127
+        |> Enum.to_list()
+        |> Enum.reduce(acc, fn row, acc ->
+          MapSet.put(acc, [row, col])
+        end)
+      end)
+
+    difference =
+      input
+      |> Enum.reduce(total_seats, fn seat, acc ->
+        row = String.slice(seat, 0..6) |> bsp(0, 0, 127)
+        col = String.slice(seat, 7..-1) |> bsp(0, 0, 7)
+        MapSet.delete(acc, [row, col])
+      end)
+
+    difference
+    |> MapSet.to_list()
+    |> Enum.sort()
+    |> Enum.each(fn [row, col] ->
+      IO.puts("#{row}, #{col}")
+    end)
   end
 end
