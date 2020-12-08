66350205859e3da03ef1997c12050f51f3089ea2
Ben Konz <benkonz@protonmail.com>
2020-12-07 22:26:04 -0500

    day 6 part 2

--- lib/day6.ex
+++ lib/day6.ex
@@ -24,5 +24,39 @@ defmodule AOC.Day6 do
 
   def part2(input) do
     input
+    |> Enum.reduce(0, fn group, acc ->
+      answers =
+        group
+        |> String.split("\n")
+        |> Enum.map(&String.graphemes(&1))
+
+      num_people = answers |> length()
+
+      answers_count =
+        answers
+        |> List.flatten()
+        |> Enum.reduce(Map.new(), fn answer, acc ->
+          Map.get_and_update(acc, answer, fn i ->
+            if i == nil do
+              {i, 1}
+            else
+              {i, i + 1}
+            end
+          end)
+          |> elem(1)
+        end)
+
+      num_answers =
+        answers_count
+        |> Enum.reduce(0, fn {_k, v}, acc ->
+          if v == num_people do
+            acc + 1
+          else
+            acc
+          end
+        end)
+
+      num_answers + acc
+    end)
   end
 end
