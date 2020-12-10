5c3aefa484d2af80d9a04b332eb2d2a15fe4517a
Ben Konz <benkonz@protonmail.com>
2020-12-09 21:48:32 -0500

    day 8 part 2

--- lib/day8.ex
+++ lib/day8.ex
@@ -18,21 +18,40 @@ defmodule AOC.Day8 do
   end
 
   def execute_instructions(instructions, pc, acc, processed_lines) do
-    if MapSet.member?(processed_lines, pc) do
-      acc
+    if pc >= length(instructions) do
+      [acc, true]
     else
-      processed_lines = MapSet.put(processed_lines, pc)
-      instruction = instructions |> Enum.at(pc)
-      [pc, acc] = execute_instruction(instruction, pc, acc)
-      execute_instructions(instructions, pc, acc, processed_lines)
+      if MapSet.member?(processed_lines, pc) do
+        [acc, false]
+      else
+        processed_lines = MapSet.put(processed_lines, pc)
+        instruction = instructions |> Enum.at(pc)
+        [pc, acc] = execute_instruction(instruction, pc, acc)
+        execute_instructions(instructions, pc, acc, processed_lines)
+      end
     end
   end
 
   def part1(input) do
-    execute_instructions(input, 0, 0, MapSet.new())
+    [acc, _finished] = execute_instructions(input, 0, 0, MapSet.new())
+    acc
   end
 
   def part2(input) do
     input
+    |> Enum.with_index()
+    |> Enum.find_value(fn {instruction, line_num} ->
+      new_instruction =
+        case instruction do
+          ["nop", arg] -> ["jmp", arg]
+          ["jmp", arg] -> ["nop", arg]
+          _ -> instruction
+        end
+
+      new_instructions = List.replace_at(input, line_num, new_instruction)
+      [acc, finished] = execute_instructions(new_instructions, 0, 0, MapSet.new())
+
+      if finished, do: acc, else: nil
+    end)
   end
 end
