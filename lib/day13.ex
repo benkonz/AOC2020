9df255472fd9f995c09d3d00e003b63af7a11978
Ben Konz <benkonz@protonmail.com>
2020-12-22 00:11:45 -0500

    day 13 part 2

--- lib/day13.ex
+++ lib/day13.ex
@@ -8,24 +8,51 @@ defmodule AOC.Day13 do
     depatures =
       departures_str
       |> String.split(",")
-      |> Enum.filter(&(&1 != "x"))
-      |> Enum.map(&String.to_integer(&1))
+      |> Enum.map(&if &1 != "x", do: String.to_integer(&1), else: nil)
 
     [timestamp |> String.to_integer(), depatures]
   end
 
-  def solve(timestamp, arrival, departures) do
+  def solve_part1(timestamp, arrival, departures) do
     found =
       departures
+      |> Enum.filter(&(&1 != nil))
       |> Enum.find_value(fn departure ->
         if rem(timestamp, departure) == 0, do: (timestamp - arrival) * departure, else: nil
       end)
 
-    if found == nil, do: solve(timestamp + 1, arrival, departures), else: found
+    if found == nil, do: solve_part1(timestamp + 1, arrival, departures), else: found
   end
 
   def part1(input) do
     [timestamp, departures] = input
-    solve(timestamp, timestamp, departures)
+    solve_part1(timestamp, timestamp, departures)
+  end
+
+  def rec({timing, offset}, lcm, timestamp) do
+    if rem(timestamp + offset, timing) != 0 do
+      rec({timing, offset}, lcm, timestamp + lcm)
+    else
+      {lcm * timing, timestamp}
+    end
+  end
+
+  def solve_part2(departures) do
+    departures
+    |> Enum.reduce({1, 0}, fn departure, {lcm, timestamp} ->
+      rec(departure, lcm, timestamp)
+    end)
+    |> elem(1)
+  end
+
+  def part2(input) do
+    [_, departures] = input
+
+    departures =
+      departures
+      |> Enum.with_index()
+      |> Enum.filter(fn {departure, _i} -> departure != nil end)
+
+    solve_part2(departures)
   end
 end
